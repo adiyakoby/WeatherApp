f5a021a2810bce7eaa8e639db274480e0cddb6fa
Adi Yakoby <adi.yakoby@gmail.com>
2024-11-27 13:30:51 +0200

    fixed server, removed unneccery .ok

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,18 +1,17 @@
 
 
 const FetchData = async (url: string) => {
-    console.log("inside fetch data");
     try {
         const response = await fetch(url);
         if (response.ok) {
             return await response.json();
         } else {
-            throw new Error("error fetching data.");
+            // throw new Error("error fetching data.");
         }
     } catch (error) {
         console.log(`Error: Couldn't fetch data from url -> ${url}`)
         console.log(error);
-        return null;
+        throw error;
     }
 
 
