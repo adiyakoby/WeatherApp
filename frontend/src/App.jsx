f983d064723d95be342f39cb6d153013b5d85f7c
Adi Yakoby <adi.yakoby@gmail.com>
2024-11-27 13:22:17 +0200

    added fetching data fuync

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputBox from './Components/InputBox'
 import WeatherBox from './Components/WeatherBox'
 import './App.css'
@@ -8,6 +8,29 @@ import 'typeface-heebo';
 
 function App() {
   const [search, setsearch] = useState('');
+  const [weatherData, setweatherData] = useState({});
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+
+  const fetchData = () => {
+    
+    if (search.length > 2) {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(search)
+      };
+
+      fetch(API_URL || 'http://localhost:5000/api', options)
+      .then(response => {
+        if (!response.ok) {
+          setweatherData({});
+        }
+        console.log(response.json());        
+      });
+    }
+  };
+
 
   return (
     
@@ -21,7 +44,7 @@ function App() {
             to see the weather<br/> 
             around the world
           </p>
-        <InputBox/>
+        <InputBox search={search} setsearch={setsearch} fetchData={fetchData}/>
       </section>
       <div className="weatherFrame">
         <aside>
